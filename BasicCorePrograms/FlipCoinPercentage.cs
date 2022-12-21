ec230176254c9954793e28b2c8b9362344a482e6
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-21 14:26:54 +0530

    FlipCoinPercentage program Uploaded

---
+++ BasicCorePrograms/FlipCoinPercentage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class FlipCoinPercentage
+    {
+        public void FlipCoinPercentageFn()
+        {
+            int tailCount = 0, headCount = 0;
+            Random random = new Random();
+            Console.WriteLine("\nEnter a number of times to flip a coin : ");
+            int numberOfTimes = Convert.ToInt32(Console.ReadLine());
+            for (int i=1; i <= numberOfTimes; i++)
+            {
+                double input = random.NextDouble();
+                if (input < 0.5)
+                {
+                    tailCount++;
+                }
+                else
+                {
+                    headCount++;
+                }
+            }
+            Console.WriteLine("Tail Count : {0} & Head Count : {1}", tailCount,headCount);
+            double tailPercentage = (tailCount *100) / numberOfTimes;
+            double headPercentage = (headCount *100) / numberOfTimes;
+            Console.WriteLine("Tail Count Percentage is : {0}% & Head Count Percentage is : {1}%", tailPercentage, headPercentage);
+
+
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs!");
+            Console.WriteLine("\n1. Flip Coin and percentage of Heads and Tails.\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoinPercentage flipCoin = new FlipCoinPercentage();
+                    flipCoin.FlipCoinPercentageFn();
+                break;
+
+                default:
+                    Console.WriteLine("Choose from given options only!");
+                break;
+            }
+
         }
     }
 }
